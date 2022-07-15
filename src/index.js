e44f0d3c7183db95fdfefecc432bf80fd2d72e2a
qa-maks-bat <qa.maksim.batrakou@gmail.com>
2022-07-15 21:44:23 +0300

    feat: created code to reverse numbers

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
-module.exports = function reverse (n) {
-  
-}
+module.exports = function reverse(n) {
+    let result = 1;
+
+    const absoluteNumber = Math.abs(n);
+    const numberToString = absoluteNumber.toString();
+
+    const numberToArray = numberToString.split("");
+
+    const reverseNumberFromArray = numberToArray.reduce(
+        function (previous, current) {
+            return current ? current + previous : previous;
+        }, "");
+
+    return result = 1 * reverseNumberFromArray;
+};
